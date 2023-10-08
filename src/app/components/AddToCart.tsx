7bea12032631e0d9db419f9a2bc2fb5df95740d0
duycoding <128799277+duycoding@users.noreply.github.com>
2023-10-08 17:22:20 +0700

    my first commit

---
+++ src/app/components/AddToCart.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react'
+
+export const AddToCart = () => {
+  return (
+    <div>
+        <button className='btn btn-primary' onClick={() => console.log('Add to cart')}>Add to cart</button>
+    </div>
+  )
+}
---
+++ src/app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+import { AddToCart } from './AddToCart';
+
+
+export const ProductCard = () => {
+  return (
+    <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500'>
+        <AddToCart></AddToCart>
+    </div>
+  )
+}
